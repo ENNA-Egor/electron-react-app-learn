098df568e02623ced9e5b28dbeaf85716c214c26
ENNA-Egor <e.yudakov@gmail.com>
2025-10-14 08:47:07 +0300

    Добавил кликер на кнопку

--- app/components/welcome/contents/NewComponent.tsx
+++ app/components/welcome/contents/NewComponent.tsx
@@ -1,13 +1,26 @@
-import React from "react"
+// import { count } from "console";
+import React, {Component} from "react"
 
 
-const NewContent = () => {
-  return (
-    <div>
-      <h1> New element in ElectronReactApp</h1>
-      <button className="btn">Click Me</button>
-    </div>
-  )
+class NewContent extends Component {
+  state = {
+      count: 0
+  };
+
+  HandelClick =() => {
+    this.setState({count: this.state.count +1})
+  } 
+  render() {
+
+
+    return (
+      <div>
+        <h1> New element in ElectronReactApp</h1>
+        <button className="btn" onClick={this.HandelClick}>{this.state.count}</button>
+      </div>
+    )
+
+  }
 }
 
 export default NewContent
